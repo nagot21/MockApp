6eb54705017a5403a055dd470548e6b235f642cd
Ian Nagot <inagot21@gmail.com>
2016-08-22 15:32:03 +0100

    Add code to layout.xml and add code to the MainActivity

--- app/src/main/java/com/nagot/mockapp/MainActivity.java
+++ app/src/main/java/com/nagot/mockapp/MainActivity.java
@@ -1,16 +1,31 @@
 package com.nagot.mockapp;
 
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout)).setTitle("teste");
+        initInstancesDrawer();
+    }
+
+    private void initInstancesDrawer(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab One"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab Two"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab Three"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab Four"));
     }
 }

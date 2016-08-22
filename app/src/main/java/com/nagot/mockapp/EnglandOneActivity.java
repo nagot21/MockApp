9fb850c356ce706b352780d10eb78ca5b831b0c6
Ian Nagot <inagot21@gmail.com>
2016-08-22 16:35:27 +0100

    Add listener to navigation icon

--- app/src/main/java/com/nagot/mockapp/EnglandOneActivity.java
+++ app/src/main/java/com/nagot/mockapp/EnglandOneActivity.java
@@ -4,10 +4,13 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
 
 public class EnglandOneActivity extends AppCompatActivity {
     ImageView imageview;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,14 @@ public class EnglandOneActivity extends AppCompatActivity {
         setContentView(R.layout.activity_england_one);
 
         ((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout)).setTitle("England 1");
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
     }
 }
